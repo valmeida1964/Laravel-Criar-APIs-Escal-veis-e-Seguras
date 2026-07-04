5d4b9433096484cc4090760d5768c03d8f84bff2
Vagner Almeida <valmeida1964@hotmail.com>
2026-07-03 22:47:38 -0300

    015

---
+++ app/Http/Controllers/Api/V1/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Services\ApiResponse;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function status()
+    {
+        return ApiResponse::success([
+            'currentStatusString' => 'API is running',
+            'serverDate' => now()->toDateString(),
+            'serverTime' => now()->toTimeString(),
+            'serverTimestamp' => now()->timestamp,
+            'serverTimezone' => now()->timezoneName,
+            'apiVersion' => 'v1 '
+
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Services\ApiResponse;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function(){
-    require base_path('routes/api_v1.php');
-});
-
-route::fallback(function(){
+Route::fallback(function(){
     return ApiResponse::error('Endpoint not found', 404);
 });
