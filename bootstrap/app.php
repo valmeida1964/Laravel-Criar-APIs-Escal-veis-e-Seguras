e1b22c213ca759a4f99d09dc470efe75aae25c0e
Vagner Almeida <valmeida1964@hotmail.com>
2026-07-04 08:17:52 -0300

    016

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\MaintenanceModeMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        
+        // check if the API is in maintenance mode
+        $middleware->api(prepend:[
+            MaintenanceModeMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
--- config/app.php
+++ config/app.php
@@ -122,5 +122,7 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
+    
+    // custom constants
+    'maintenance_mode' => env('APP_MAINTENANCE_MODE', false),
 ];
